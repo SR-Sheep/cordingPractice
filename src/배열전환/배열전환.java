75c8fbfd7593628c1ea216320c74916cd7a51dce
양성룡 <act4ysr@gmail.com>
2021-10-21 10:58:54 +0900

    배열전환

--- src/배열전환/배열전환.java
+++ src/배열전환/배열전환.java
@@ -2,7 +2,7 @@ package 배열전환;
 
 public class 배열전환 {
 	public static void main(String[] args) {
-		String s = "[93, 30, 55]";
+		String s = "";
 		s=s.replace("[", "{").replace("]", "}");
 		System.out.println(s);
 	}
