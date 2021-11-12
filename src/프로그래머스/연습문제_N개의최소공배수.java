c51611b211ae725d6c02e1d427f9b3ceda87d04b
양성룡 <act4ysr@gmail.com>
2021-11-12 15:09:00 +0900

    n개의 공배수

---
+++ src/프로그래머스/연습문제_N개의최소공배수.java
@@ -0,0 +1,39 @@
+package 프로그래머스;
+
+public class 연습문제_N개의최소공배수 {
+    //최대공약수
+    public static int gcd(int a, int b){
+        int big=0;
+        int small=0;
+        if(a>b){
+            big=a;
+            small=b;
+        }else{
+            big=b;
+            small=a;
+        }
+        if(small==0) return big;
+        else return gcd(small,big%small);
+    }
+    //최소공배수
+    public static int lcd(int a, int b){
+        return a*b/gcd(a,b);
+    }
+    
+    
+    public static int solution(int[] arr) {
+        int answer = 1;
+        for(int i:arr){
+            answer=lcd(answer,i);
+        }
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		int n=2;
+		int[][] arr = {{2,6,8,14},{1,2,3}}; //168, 6
+		for(int i=0;i<n;i++) {
+			System.out.println(solution(arr[i]));
+		}
+	}
+}
