9fe452c7a8c50b95f2a5eb3f177aa46e93fe6014
양성룡 <act4ysr@gmail.com>
2021-11-17 21:58:23 +0900

    백준 기본, 빠른 출력을 위한 BufferedWriter 추가

--- src/백준/기본_백준.java
+++ src/백준/기본_백준.java
@@ -1,15 +1,21 @@
 package 백준;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class 기본_백준 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		br.close();
+		bw.write(n+"\n");
+		bw.close();
+		
 	}
 }
