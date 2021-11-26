28a824780d3f37806337da45e2f66bc303616baf
양성룡 <act4ysr@gmail.com>
2021-11-27 00:20:21 +0900

    우선순위 큐

---
+++ src/프로그래머스/연습문제_야근지수.java
@@ -0,0 +1,45 @@
+package 프로그래머스;
+
+import java.util.PriorityQueue;
+
+public class 연습문제_야근지수 {
+	
+	//https://programmers.co.kr/learn/courses/30/lessons/12927
+	/*
+	회사원 Demi는 가끔은 야근을 하는데요, 야근을 하면 야근 피로도가 쌓입니다.
+	야근 피로도는 야근을 시작한 시점에서 남은 일의 작업량을 제곱하여 더한 값입니다.
+	Demi는 N시간 동안 야근 피로도를 최소화하도록 일할 겁니다.
+	Demi가 1시간 동안 작업량 1만큼을 처리할 수 있다고 할 때,
+	퇴근까지 남은 N 시간과 각 일에 대한 작업량 works에 대해 야근 피로도를 최소화한 값을 리턴하는 함수 solution을 완성해주세요.
+
+	제한 사항
+	works는 길이 1 이상, 20,000 이하인 배열입니다.
+	works의 원소는 50000 이하인 자연수입니다.
+	n은 1,000,000 이하인 자연수입니다.
+	*/
+	
+    public static long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>((x,y)->(y-x));
+        for(int i:works){
+            q.add(i);
+        }
+        while(n>0){
+            q.add(q.poll()-1);
+            n--;
+        }
+        while(!q.isEmpty()){
+            int work = q.poll();
+            if(work<0) return 0;
+            answer+=work*work;
+        }
+        
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		int[] works = {4,3,3};
+		int n = 4;
+		System.out.println(solution(n, works));
+	}
+}
