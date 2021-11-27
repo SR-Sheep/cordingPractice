587eae874ce6f49e0ea417eaee1d671b33f53db1
양성룡 <act4ysr@gmail.com>
2021-11-28 01:07:44 +0900

    수학

---
+++ src/백준/부녀회장이될꺼야.java
@@ -0,0 +1,33 @@
+package πÈ¡ÿ;
+
+import java.io.*;
+import java.util.*;
+
+public class ∫Œ≥‡»∏¿Â¿Ãµ…≤®æﬂ {
+	
+	//https://www.acmicpc.net/problem/2775
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int repeat = Integer.parseInt(br.readLine());
+		
+		int[][] board=new int[15][15];
+		for(int i=0;i<15;i++) {
+			board[0][i]=i+1;
+			board[i][0]=1;
+		}
+		for(int i=1;i<15;i++) {
+			for(int j=1;j<15;j++) {
+				board[i][j]= board[i-1][j]+board[i][j-1];
+			}
+		}
+		for(int i=0;i<repeat;i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine())-1;
+			bw.append(board[k][n]+"\n");
+		}
+		br.close();
+		bw.close();
+	}
+}
