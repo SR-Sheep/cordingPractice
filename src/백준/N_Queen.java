30ff1976633d1488ad8b7611c0c38be7bf9e85d8
양성룡 <act4ysr@gmail.com>
2021-12-05 00:50:20 +0900

    백트래킹 퀸 체스

---
+++ src/백준/N_Queen.java
@@ -0,0 +1,41 @@
+package น้มุ;
+
+import java.io.*;
+import java.util.*;
+
+public class N_Queen {
+	
+	static int N,Answer;
+	static boolean[] VisitedRow;
+	static boolean[] VisitedRU;
+	static boolean[] VisitedLU;
+	
+	public static void dfs (int idx){
+		if(idx>=N) {
+			Answer++;
+			return;
+		}
+		for(int i=0;i<N;i++) {
+			if(VisitedRow[i]) continue;
+			if(VisitedRU[i+idx]) continue;
+			if(VisitedLU[N-i+idx]) continue;
+			VisitedRow[i]=VisitedRU[i+idx]=VisitedLU[N-i+idx]=true;
+			dfs(idx+1);
+			VisitedRow[i]=VisitedRU[i+idx]=VisitedLU[N-i+idx]=false;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N=Integer.parseInt(br.readLine());
+		VisitedRow=new boolean[N];
+		VisitedRU=new boolean[N*2];
+		VisitedLU=new boolean[N*2];
+		Answer=0;
+		dfs(0);
+		bw.append(Answer+"\n");
+		br.close();
+		bw.close();
+	}
+}
