20086ffb68ac55a7680f9b1d81a278dd22de55e9
양성룡 <act4ysr@gmail.com>
2021-12-09 22:38:01 +0900

    stack

---
+++ src/백준/제로.java
@@ -0,0 +1,30 @@
+package 백준;
+
+import java.io.*;
+import java.util.Stack;
+
+public class 제로 {
+	//https://www.acmicpc.net/problem/10773
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		int num = 0;
+		int sum = 0;
+		for(int i=0;i<n;i++) {
+			num = Integer.parseInt(br.readLine());
+			if(num!=0) {
+				sum+=num; //0이 아니면 더하기
+				stack.add(num); //스택에 수 저장
+			}else {
+				sum-=stack.pop(); //0이면 스택에서 뽑아서 뺴기
+			}
+		}
+		bw.append(sum+"\n");
+		br.close();
+		bw.close();
+	}
+}
