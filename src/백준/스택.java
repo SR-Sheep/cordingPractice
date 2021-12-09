2fbd33aa372ec1f8872b8b673e1c27475150192d
양성룡 <act4ysr@gmail.com>
2021-12-09 22:37:22 +0900

    stack

---
+++ src/백준/스택.java
@@ -0,0 +1,57 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 스택 {
+	//https://www.acmicpc.net/problem/10828
+	
+	static Stack<Integer> stack;
+	static StringTokenizer st;
+	static BufferedWriter bw;
+	
+	public static void command() throws IOException {
+		String cmd = st.nextToken();
+		if(cmd.equals("push")) {
+			stack.add(Integer.parseInt(st.nextToken())); //push
+		}else if(cmd.equals("pop")) {
+			if(stack.isEmpty()) {
+				bw.append(-1+"\n"); //비어있으면 -1
+			}else {
+				bw.append(stack.pop()+"\n"); //pop
+			}
+		}else if(cmd.equals("size")) {
+			bw.append(stack.size()+"\n"); //size
+		}else if(cmd.equals("empty")) {
+			if(stack.isEmpty()) {
+				bw.append(1+"\n"); //비어있으면 1
+			}else {
+				bw.append(0+"\n"); //비어있지 않으면 0
+			}
+		}else {
+			if(stack.isEmpty()) {
+				bw.append(-1+"\n"); //비어있으면 -1
+			}else {
+				bw.append(stack.peek()+"\n"); //top
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		stack = new Stack<Integer>();
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {
+			st=new StringTokenizer(br.readLine());
+			command();
+		}
+		br.close();
+		bw.close();
+	}
+}
