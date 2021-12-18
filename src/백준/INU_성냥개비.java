493541317027d5ad446aa4b6b47d690387b68d0c
양성룡 <act4ysr@gmail.com>
2021-12-19 01:40:39 +0900

    브루트포스 알고리즘

---
+++ src/백준/INU_성냥개비.java
@@ -0,0 +1,42 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class INU_성냥개비 {
+	//https://www.acmicpc.net/problem/23842
+	
+	static int[] match = {6,2,5,5,4,5,6,3,7,6};
+	
+	public static String solution(int n) {
+		n-=4; //+, = 성냥 사용
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<10;j++) {
+				for(int a=0;a<10;a++) {
+					for(int b=0;b<10;b++) {
+						for(int c=0;c<10;c++) {
+							for(int d=0;d<10;d++) {
+								if(match[i]+match[j]+match[a]+match[b]+match[c]+match[d]==n&&i*10+j+a*10+b==c*10+d) {
+									return i+""+j+"+"+a+""+b+"="+c+""+d;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return "impossible";
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		bw.append(solution(n));
+		br.close();
+		bw.close();
+	}
+}
