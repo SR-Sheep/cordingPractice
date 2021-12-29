611e668481628e2abea67ecf8791170f726223cb
양성룡 <act4ysr@gmail.com>
2021-12-29 13:33:12 +0900

    동적계획법

---
+++ src/백준/포도주시식.java
@@ -0,0 +1,26 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 포도주시식 {
+	//https://www.acmicpc.net/problem/2156
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] wines = new int[n+1];
+		int[] dp = new int[n+1];
+		for(int i=1;i<=n;i++) {
+			wines[i]=Integer.parseInt(br.readLine());
+		}
+		dp[1]=wines[1];
+		if(n>1) dp[2]=wines[1]+wines[2];
+		for(int i=3;i<=n;i++) {
+			dp[i]=wines[i]+Math.max(dp[i-3]+wines[i-1], dp[i-2]); //3전 + 1전 , 2전
+			dp[i]=Math.max(dp[i], dp[i-1]); //이전과 비교하여 큰 값을 취함(현재의 포도주를 안 마실수도 있음)
+		}
+		System.out.println(dp[n]);
+		br.close();
+	}
+}
