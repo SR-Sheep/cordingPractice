449abf516437da3a4a158d8d9b6e04efd42a6e72
양성룡 <act4ysr@gmail.com>
2022-01-03 23:27:21 +0900

    문자열

--- src/백준/팰린드롬수.java
+++ src/백준/팰린드롬수.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class ÆÓ¸°µå·Ò¼ö {
+	//https://www.acmicpc.net/problem/1259
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
