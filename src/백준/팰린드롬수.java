ac7d5f98c3bf77483fe71c17c11749e792d3afeb
양성룡 <act4ysr@gmail.com>
2022-01-03 23:26:47 +0900

    문자열

---
+++ src/백준/팰린드롬수.java
@@ -0,0 +1,28 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 팰린드롬수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String s = br.readLine();
+		while(!s.equals("0")) {
+			boolean isPalindrome = true;
+			for(int i=0;i<s.length();i++) {
+				if(s.charAt(i)!=s.charAt(s.length()-1-i)) {
+					isPalindrome=false;
+					break;
+				}
+			}
+			bw.append(isPalindrome?"yes\n":"no\n");
+			s=br.readLine();
+		}
+		br.close();
+		bw.close();
+	}
+}
