2c38bb6bd14b2eee8736904fa836027bcb63695e
양성룡 <act4ysr@gmail.com>
2022-01-09 23:42:47 +0900

    큐 그래프 bfs

--- src/백준/트리의부모찾기.java
+++ src/백준/트리의부모찾기.java
@@ -6,27 +6,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 트리의부모찾기 {
-	
+	//https://www.acmicpc.net/problem/11725
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int t = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		int[] nodes = new int[t+1];
-		List<Integer> children = new ArrayList<Integer>();
+		List<Integer>[] list = new ArrayList[t+1]; //인접 리스트
+		for(int i=0;i<t+1;i++) {
+			list[i] = new ArrayList<>();
+		}
+		boolean[] visited = new boolean[t+1]; //방문 여부
+		int[] parents = new int[t+1]; //부모 기록
+		//리스트에 입력
 		for(int i=0;i<t-1;i++) {
 			st=new StringTokenizer(br.readLine());
-			int parent=Integer.parseInt(st.nextToken());
-			int child=Integer.parseInt(st.nextToken());
-			nodes[child]=parent;
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
 		}
-		
+		//큐 선언
+		Queue<Integer> q = new LinkedList<>();
+		q.add(1); //루트(1) 입력
+		while(!q.isEmpty()) {
+			int curr = q.poll();
+			visited[curr]=true; //방문 여부 표시
+			//인접 리스트 탐색
+			for(int i:list[curr]) {
+				if(visited[i]) continue; //방문시 패스
+				parents[i]=curr; //부모 기록
+				q.add(i);
+			}
+		}
 		for(int i=2;i<=t;i++) {
-			bw.append(nodes[i]+"\n");
+			bw.append(parents[i]+"\n"); //2번부터 부모 출력
 		}
 		br.close();
 		bw.close();
