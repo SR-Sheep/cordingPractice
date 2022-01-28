c99ca4cd61ce46b9d74ad135f68585e6272368fe
양성룡 <act4ysr@gmail.com>
2022-01-28 23:51:38 +0900

    숫자

---
+++ src/백준/일구구팔년생인내가태국에서는2541년생.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 일구구팔년생인내가태국에서는2541년생 {
+	//https://www.acmicpc.net/problem/18108
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine())-543);
+		br.close();
+	}
+}
