9f702ccd4146e2dc54a07c931fa683351262c995
양성룡 <act4ysr@gmail.com>
2022-03-09 23:51:01 +0900

    그리디

--- src/백준/보물.java
+++ src/백준/보물.java
@@ -1,4 +1,4 @@
-package ����;
+package 백준;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class ���� {
+public class 보물 {
 	//https://www.acmicpc.net/problem/1026
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,9 +21,12 @@ public class ���� {
 		for(int i=0;i<t;i++) {
 			bArr[i]=Integer.parseInt(st.nextToken());
 		}
+		//a 재배열
 		Arrays.sort(aArr);
+		//b 재배열, 문제에서는 b는 재배열 하면 안된다고 했지만, 결과값을 내기에는 상관없음
 		Arrays.sort(bArr);
 		long sum = 0;
+		//a의 최소값 * b의 최대값 의 합
 		for(int i=0;i<t;i++) {
 			sum+=aArr[i]*bArr[t-1-i];
 		}
