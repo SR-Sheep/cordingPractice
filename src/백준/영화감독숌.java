235dde3207176648b7522fd87805076767d3933c
양성룡 <act4ysr@gmail.com>
2022-04-16 23:52:28 +0900

    완전탐색

--- src/백준/영화감독숌.java
+++ src/백준/영화감독숌.java
@@ -1,33 +1,25 @@
-package ¹éÁØ;
+package ë°±ì¤€;
 
 import java.io.*;
 import java.util.*;
 
-public class ¿µÈ­°¨µ¶¼ò {
+public class ì˜í™”ê°ë…ìˆŒ {
+	//https://www.acmicpc.net/problem/1436
+	
 	public static int solution(int n) {
 		int num=666;
-		List<Integer> list = new ArrayList<Integer>();
-		while(list.size()<n) {
-			if(Integer.toString(num).contains("666")) list.add(num);
+		int count = 0;
+		while(count<n) {
+			if(Integer.toString(num).contains("666")) count++;
 			num++;
 		}
-		return list.get(n-1);
+		return num-1;
 	}
 	
 	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		int n = Integer.parseInt(st.nextToken());
-//		br.close();
-//		System.out.println(solution(n));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<n;i++) list.add(Integer.parseInt(br.readLine()));
+        System.out.println(solution(n));
         br.close();
-        Collections.sort(list);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i:list) bw.write(i+"\n");;
-		bw.close();
 	}
 }
