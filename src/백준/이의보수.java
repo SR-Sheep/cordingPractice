0c340eb2501360fe5a37f422beff7c9e48204b9b
양성룡 <act4ysr@gmail.com>
2022-04-17 15:55:21 +0900

    2의 보수

---
+++ src/백준/이의보수.java
@@ -0,0 +1,17 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이의보수 {
+	https://www.acmicpc.net/problem/24389
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int tmp = ~num+1;
+		int answer = num^tmp;
+		System.out.println(Integer.bitCount(answer));
+		br.close();
+	}
+}
