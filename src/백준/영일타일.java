8862476e6654902b4de3c967bb3892023a407a4e
양성룡 <act4ysr@gmail.com>
2022-04-17 16:40:07 +0900

    다이나믹 프로그래밍

--- src/백준/영일타일.java
+++ src/백준/영일타일.java
@@ -1,4 +1,4 @@
-package ����;
+package 백준;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,22 +6,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class ����Ÿ�� {
+public class 영일타일 {
 	//https://www.acmicpc.net/problem/1904
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n+1];
+		//n이 1개면 1
 		if(n==1) {
 			bw.append(1+"\n");
 			bw.close();
 			br.close();
 			return;
 		}
-		arr[1]=1;
-		arr[2]=2;
+		arr[1]=1; //1
+		arr[2]=2; //00, 11
 		for(int i=3;i<=n;i++) {
+			//앞에 1 타일이 올 경우(i-1) + 앞에 00 타일이 올 경우(i-2)
 			arr[i]=(arr[i-1]+arr[i-2])%15746;
 		}
 		bw.append(arr[n]+"\n");
