f864a6b585a5882a9c13bbd5e56769204d5c1752
양성룡 <act4ysr@gmail.com>
2022-04-17 17:28:22 +0900

    2의 보수, 출처 추가

--- src/백준/이의보수.java
+++ src/백준/이의보수.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 이의보수 {
-	https://www.acmicpc.net/problem/24389
+//	https://www.acmicpc.net/problem/24389
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
