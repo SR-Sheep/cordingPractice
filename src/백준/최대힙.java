d07fa74a220fff22b51fd5077f9c100e0784efb8
양성룡 <act4ysr@gmail.com>
2022-04-22 21:30:56 +0900

    우선순위큐

--- src/백준/최대힙.java
+++ src/백준/최대힙.java
@@ -1,4 +1,4 @@
-package ����;
+package 백준;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,12 +7,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.PriorityQueue;
 
-public class �ִ��� {
+public class 최대힙 {
 	//https://www.acmicpc.net/problem/11279
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
+		//우선순위큐, 최대값부터 poll
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x,y)->(y-x));
 		for(int i=0;i<n;i++) {
 			int num = Integer.parseInt(br.readLine());
