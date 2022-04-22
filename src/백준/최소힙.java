a58b1116ece72d8751072229da710281a3e53a7f
양성룡 <act4ysr@gmail.com>
2022-04-22 21:32:21 +0900

    우선순위큐

--- src/백준/최소힙.java
+++ src/백준/최소힙.java
@@ -1,4 +1,4 @@
-package ����;
+package 백준;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,12 +7,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.PriorityQueue;
 
-public class �ּ��� {
+public class 최소힙 {
 	//https://www.acmicpc.net/problem/1927
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
+		//우선순위 큐, 작은 값부터 출력(기본값)
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 		for(int i=0;i<n;i++) {
 			int num = Integer.parseInt(br.readLine());
