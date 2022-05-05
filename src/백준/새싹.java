54fd2ecda8b2b08318a2810479fc53c22131feee
양성룡 <act4ysr@gmail.com>
2022-05-05 13:07:37 +0900

    문자열 출력, \", \\

---
+++ src/백준/새싹.java
@@ -0,0 +1,14 @@
+package 백준;
+
+public class 새싹 {
+	//https://www.acmicpc.net/problem/25083
+	public static void main(String[] args) {
+		String s ="         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |\n";
+		System.out.println(s);
+	}
+}
