1242069595005186fb857a1de96df2f9d6a8cbca
양성룡 <act4ysr@gmail.com>
2022-06-20 23:59:23 +0900

    구현

---
+++ src/백준/서강의역사를찾아서.java
@@ -0,0 +1,38 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 서강의역사를찾아서 {
+	//https://www.acmicpc.net/problem/25177
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(st.nextToken()); //현재 시설 수
+		int m=Integer.parseInt(st.nextToken()); //과거 시설 수
+		
+		int max = Math.max(n,m); //둘 중 최대값
+		
+		int[] befores = new int[max]; //과거 건물 점수 배열
+		int[] nows = new int[max]; //현재 건물 점수 배열
+		//현재 건물 점수 입력
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			nows[i]=Integer.parseInt(st.nextToken());
+		}
+		//과거 건물 점수 입력
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<m;i++) {
+			befores[i]=Integer.parseInt(st.nextToken());
+		}
+		//점수 차 최대값 출력
+		int answer = 0;
+		for(int i=0;i<max;i++) {
+			answer=Math.max(answer, nows[i]-befores[i]);
+		}
+		System.out.println(answer);
+		br.close();
+	}
+}
