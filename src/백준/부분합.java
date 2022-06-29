53f187c659992de2448d07712e1f6823e9af7130
양성룡 <act4ysr@gmail.com>
2022-06-29 23:59:39 +0900

    부분합, 이분탐색

--- src/백준/부분합.java
+++ src/백준/부분합.java
@@ -11,35 +11,41 @@ public class 부분합 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st=new StringTokenizer(br.readLine());
-		int n=Integer.parseInt(st.nextToken());
-		int s=Integer.parseInt(st.nextToken());
+		int n=Integer.parseInt(st.nextToken()); //
+		int s=Integer.parseInt(st.nextToken()); //도달해야 하는 합
 		st=new StringTokenizer(br.readLine());
-		int[] board = new int[n];
+		int[] board = new int[n];//배열
 		int max = 0;
+		//배열 넣기
 		for(int i=0;i<n;i++) {
 			board[i]=Integer.parseInt(st.nextToken());
 			max=Math.max(max, board[i]);
 		}
+		//max가 s보다 크면 한개로 충분함
 		if(max>=s) {
 			System.out.println(1);
 		}else {
+			//최대 길이
 			int length = 100001;
 			int left = 0;
 			int right = 1;
 			long sum = board[left];
 			while(right<n) {
+				//합 계산, right 증가
 				sum+=board[right++];
+				//합이 S보다 크다면
 				if(sum>=s) {
+					//sum이 s보다 작을 때까지 계산
 					while(sum>=s) {
 						sum-=board[left++];
 						if(left==right) break;
 					}
+					//길이
 					length = Math.min(length, right-left+1);
 				}
 			}
 			System.out.println(length==100001?0:length);
 		}
-		
 		br.close();
 	}
 }
