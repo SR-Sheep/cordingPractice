411817e9eb399e7de30bc49bee8706c3ee2578aa
양성룡 <act4ysr@gmail.com>
2022-07-05 23:58:56 +0900

    구현

---
+++ src/백준/UCPC에서가장쉬운문제번호는.java
@@ -0,0 +1,16 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class UCPC에서가장쉬운문제번호는 {
+	//https://www.acmicpc.net/problem/25311
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println("A");
+		br.close();
+	}
+}
