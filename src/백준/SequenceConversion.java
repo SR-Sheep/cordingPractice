fe78e669c3d57fa37cc068788585514516d7a68b
양성룡 <act4ysr@gmail.com>
2023-01-22 23:46:22 +0900

    xor 기존 코드 구체화

--- src/백준/SequenceConversion.java
+++ src/백준/SequenceConversion.java
@@ -10,10 +10,14 @@ public class SequenceConversion {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//배열 길이
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st=new StringTokenizer(br.readLine());
+		//a 배열
 		long[] befores =new long[n];
+		//b 배열
 		long[] afters =new long[n];
+		//값 입력
 		for(int i=0;i<n;i++) {
 			befores[i]=Long.parseLong(st.nextToken());
 		}
@@ -21,23 +25,27 @@ public class SequenceConversion {
 		for(int i=0;i<n;i++) {
 			afters[i]=Long.parseLong(st.nextToken());
 		}
-		
+		//연산 횟수
 		int count=0;
-		
+		//탐색, i번째 연산은 i+1에 영향을 주기에 n-1번째까지만 반복
 		for(int i=0;i<n-1;i++) {
 			long before = befores[i];
 			long after = afters[i];
+			//전과 후가 다르다면
 			if(before!=after) {
+				//횟수 증가
 				count++;
+				//xor 할 x 구하기
 				long x = before^after;
-				befores[i]=after;
-				befores[i+1]=befores[i+1]^x;
+				//변환 전의 i+1에 x와 xor 연산
+				befores[i+1]^=x;
 			}
 		}
-		
+		//마지막 수가 일치되지 않았다면 -1
 		if(befores[n-1]!=afters[n-1]) {
 			count=-1;
 		}
+		//출력
 		System.out.println(count);
 		br.close();
 	}
