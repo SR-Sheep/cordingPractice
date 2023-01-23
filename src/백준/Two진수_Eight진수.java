6b0375b28e665726f17ee57ef3d7eecc80076d09
양성룡 <act4ysr@gmail.com>
2023-01-24 00:19:29 +0900

    100만자리수인 경우에도 작동하도록 수정

--- src/백준/Two진수_Eight진수.java
+++ src/백준/Two진수_Eight진수.java
@@ -3,13 +3,29 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Two진수_Eight진수 {
 	//https://www.acmicpc.net/problem/1373
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long t = Long.parseLong(br.readLine(),2); //2진수 -> 10진수
-		System.out.println(Long.toOctalString(t));//10진수 -> 8진수
+		String number = br.readLine();
+		Stack<Integer> stack = new Stack<>();
+		for(char c:number.toCharArray()) stack.push(Integer.parseInt(c+"")); //스택에 넣기
+		StringBuilder sb = new StringBuilder(); //답 출력할 스트링빌더
+		//1의자리수부터 탐색
+		while(!stack.isEmpty()) {
+			int sum = 0; //3자리씩 끊어서 2진수를 10진수로 변환
+			if(stack.size()>2) { //3개라면
+				sum = stack.pop() + stack.pop()*2 + stack.pop()*4;
+			}else if(stack.size()>1) { //2개라면
+				sum = stack.pop() + stack.pop()*2;
+			}else { //1개라면
+				sum = stack.pop();
+			}
+			sb.append(Integer.toOctalString(sum)); //8진수로 변환하여 뒤에 붙이기
+		}
+		System.out.println(sb.reverse()); //sb 거꾸로 출력
 		br.close();
 	}
 }
