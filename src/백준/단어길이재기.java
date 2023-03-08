1f871b5109a4bdb57a042dc8c3c381d77913a808
양성룡 <act4ysr@gmail.com>
2023-03-08 23:41:58 +0900

    2743 문자열 길이

---
+++ src/백준/단어길이재기.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 단어길이재기 {
+	//https://www.acmicpc.net/problem/2743
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().length());
+		br.close();
+	}
+}
