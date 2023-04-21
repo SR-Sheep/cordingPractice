8ef4af56f548c4287b2e581d3ffbb663e2c0fa7d
양성룡 <act4ysr@gmail.com>
2023-04-22 00:03:17 +0900

    1271 BigInteger 몫과 나머지

---
+++ src/백준/엄청난부자2.java
@@ -0,0 +1,21 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class 엄청난부자2 {
+	//https://www.acmicpc.net/problem/1271
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		BigInteger n = new BigInteger(st.nextToken());
+		BigInteger m = new BigInteger(st.nextToken());
+		BigInteger[] result =  n.divideAndRemainder(m);
+		System.out.println(result[0]); //몫
+		System.out.println(result[1]); //나머지
+		br.close();
+	}
+}
