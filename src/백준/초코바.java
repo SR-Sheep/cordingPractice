d69a7ac46dfaff8c0aa6539c56d489b051f378a2
양성룡 <act4ysr@gmail.com>
2023-05-17 23:56:27 +0900

    27959 구현

---
+++ src/백준/초코바.java
@@ -0,0 +1,17 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//https://www.acmicpc.net/problem/27959
+public class 초코바 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		System.out.println(n*100>=m?"Yes":"No");
+		br.close();
+	}
+}
