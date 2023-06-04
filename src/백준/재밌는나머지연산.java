d132985215cea35f48e8d1fd6c3da396538f55f6
양성룡 <act4ysr@gmail.com>
2023-06-04 14:31:30 +0900

    23138 나머지연산 최적화

--- src/백준/재밌는나머지연산.java
+++ src/백준/재밌는나머지연산.java
@@ -6,20 +6,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 재밌는나머지연산 {
+	//https://www.acmicpc.net/problem/28138
 	
 	public static long getRemainderSum(long n, long r) {
 		long sum = 0;
-		long tmp = n - r;
+		long tmp = n - r; //n-r을 한 수의 약수 중 조건에 해당되는 경우 합
 		for(long i=1;i*i<=tmp;i++) {
 			if(tmp%i==0) {
+				//i로 n을 나누었을 때, r인 경우
 				if(n%i==r) {
 					sum+=i;
 				}
+				//i의 파트너로 n을 나누었을 때, r인 경우
+				//(단 i*i 가 tmp 인경우 중복임으로 제외)
 				if(i*i!=tmp && n%(tmp/i)==r) {
 					sum+=tmp/i;
 				}
 			}
 		}
+		//출력
         return sum;
 	} 
 	
